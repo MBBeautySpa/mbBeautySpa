3f002743160137c5511479bb86738549c991fe17
Kevin <kevinvar2805@gmail.com>
2025-05-09 14:46:54 -0500

    Added NavBar and general template for the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
 
-const App: React.FC = () => {
+import { Routes, Route } from 'react-router';
+import Home from './pages/Home/Home.tsx';
+import NavBar from './components/NavBar.tsx';
+const App = () => {
   return (
-    <div className="app"></div>
+    <div className="bg-pink-100" id="app">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<div>about</div>} />
+        <Route path="/services" element={<div>services</div>} />
+        <Route path="/gallery" element={<div>gallery</div>} />
+      </Routes>
+    </div>
+
   )
 }
 
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-pink-100">
+      <h1 className="text-4xl font-bold text-pink-600">Welcome to MB Beauty Spa</h1>
+      <p className="mt-4 text-lg text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    </div>
+  )
+}
+
+export default Home;
