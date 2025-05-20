4b4ef2968cc628514c50f49b05a366e3e4b4632a
Kevin <kevinvar2805@gmail.com>
2025-05-20 13:53:43 -0500

    Changed to actual imports instead of dynamic

--- src/pages/Gallery/gallery.ts
+++ src/pages/Gallery/gallery.ts
@@ -1,10 +1,59 @@
+import img0 from '@/assets/gallery/0.jpeg';
+import img1 from '@/assets/gallery/1.jpeg';
+import img2 from '@/assets/gallery/2.jpeg';
+import img3 from '@/assets/gallery/3.jpeg';
+import img4 from '@/assets/gallery/4.jpeg';
+import img5 from '@/assets/gallery/5.jpeg';
+import img6 from '@/assets/gallery/6.jpeg';
+import img7 from '@/assets/gallery/7.jpeg';
+import img8 from '@/assets/gallery/8.jpeg';
+import img9 from '@/assets/gallery/9.jpeg';
+import img10 from '@/assets/gallery/10.jpeg';
+import img11 from '@/assets/gallery/11.jpeg';
+import img12 from '@/assets/gallery/12.jpeg';
+import img13 from '@/assets/gallery/13.jpeg';
+import img14 from '@/assets/gallery/14.jpeg';
+import img15 from '@/assets/gallery/15.jpeg';
+import img16 from '@/assets/gallery/16.jpeg';
+import img17 from '@/assets/gallery/17.jpeg';
+import img18 from '@/assets/gallery/18.jpeg';
+import img19 from '@/assets/gallery/19.jpeg';
+import img20 from '@/assets/gallery/20.jpeg';
+import img21 from '@/assets/gallery/21.jpeg';
+import img22 from '@/assets/gallery/22.jpeg';
+import img23 from '@/assets/gallery/23.jpeg';
+import img24 from '@/assets/gallery/24.jpeg';
+import img25 from '@/assets/gallery/25.jpeg';
+import img26 from '@/assets/gallery/26.jpeg';
 
-const images: string[] = [];
-for (let i = 0; i < 27; i++) {
-  import('../../assets/gallery/' + i + '.jpeg').then((img) => {
-    images.push(img.default);
-  });
-
-}
+const images: string[] = [
+  img0,
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+  img19,
+  img20,
+  img21,
+  img22,
+  img23,
+  img24,
+  img25,
+  img26,
+];
 
 export default images;
